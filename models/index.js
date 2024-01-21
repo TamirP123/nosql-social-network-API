ce70f4d3a4d9389d12555d3ff7fcb9073d8c8510
Tamir <tamirphillips@live.com>
2024-01-21 09:35:00 -0500

    Finished up most of reaction model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const User = require("./User");
-const Tag = require("./Thought");
-const Application = require("./Application");
+const Thought = require("./Thought");
+const Reaction = require("./Reaction");
 
-module.exports = { User, Application, Tag };
+module.exports = { User, Thought, Reaction };
